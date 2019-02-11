990c7b8f6d5abaa5a92e2acd28bc13969d5158f8
bokulbacki <bokulbacki@bos-macbook-pro>
2019-02-10 20:41:10 -0700

    final final final! I added the period changer. all good. no mas

--- StudentInformationSorting/src/GradesChange.java
+++ StudentInformationSorting/src/GradesChange.java
@@ -39,5 +39,44 @@ public class GradesChange
 			SetGPA.setGPA();
 			PrintStudents.printStudents();	
 		}
+		
+		public static void scheduleChange()
+		{
+			System.out.println("Which student's schedule would you like to change?");
+			PrintStudents.printStudents();
+			Scanner userInput = new Scanner(System.in);
+			int changes = userInput.nextInt();
+			
+			System.out.println("You chose " + SISRunner.students.get(changes-1).getFirstName()+".");
+			
+			System.out.println("Which period would you like to change?");
+			Scanner userInput1 = new Scanner(System.in);
+			int period = userInput1.nextInt();
+			System.out.println("What do you want the new class to be?");
+			Scanner userInput2 = new Scanner(System.in);
+			String grades = userInput2.nextLine();
+			System.out.println("Please input the grade for " + grades + " class.");
+			String newClassGrade = userInput2.nextLine();
+			if(period == 1)
+			   {
+				    
+				    SISRunner.students.get(changes-1).setPeriod1(grades);
+				    SISRunner.students.get(changes-1).setGrade1(newClassGrade);
+			   }
+			else if(period == 2)
+				{
+					SISRunner.students.get(changes-1).setPeriod2(grades);
+					SISRunner.students.get(changes-1).setGrade2(newClassGrade);
+				}
+			else if(period == 3)
+				{
+					SISRunner.students.get(changes-1).setPeriod3(grades);
+					SISRunner.students.get(changes-1).setGrade3(newClassGrade);
+				}
+			
+			System.out.println("You just changed " + SISRunner.students.get(changes-1).getFirstName() +  "'s "+ "period " + period);
+			SetGPA.setGPA();
+			PrintStudents.printStudents();
+		}
 
 	}
